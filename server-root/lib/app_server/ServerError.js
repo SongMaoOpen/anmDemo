177eb90d1b71132d285d57ff6026d4facbd6ef6c
Sebastian Qu <sebastianqu@gmail.com>
2016-01-26 16:43:58 +0800

    add missing

--- server-root/lib/app_server/ServerError.js
+++ server-root/lib/app_server/ServerError.js
@@ -26,6 +26,8 @@ module.exports = {
     ERR_INVALID_USER: new ServerError(9001, 'Username or password invaild.'),
     ERR_NOT_ENOUGH_PARAM: new ServerError(9002, 'Parameter is not enough.'),
     ERR_USER_IS_EXISTS: new ServerError(9003, 'User is exists.'),
+    ERR_TOKEN: new ServerError(9004, 'Invalid token.'),
+    ERR_USER_IS_NOT_EXISTS: new ServerError(9005, 'User is not exists.'),
     ERR_UKNOWN: new ServerError(9999, 'Unknown Error.'),
     getUnKnownError: _getUnKnownError
 };
