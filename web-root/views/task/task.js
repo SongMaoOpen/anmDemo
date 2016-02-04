f1ce6fe3fc86dde8493d7e118c4f6006faf01d7e
lidasi <844133353@qq.com>
2016-02-04 17:28:57 +0800

    分页bug修复，

--- web-root/views/task/task.js
+++ web-root/views/task/task.js
@@ -27,6 +27,8 @@
 	var obj;
     var currentPage = 1;
     var max;
+    
+    
     var token = window.localStorage.getItem('token');
     $http.get(config.apiUrl + 'user', {
         headers: {
@@ -43,6 +45,7 @@
                 
             $http.post(config.apiUrl + 'task/show', taskId).then(function(res){
                    // $scope.rsData = res.data[0]; 
+                   
                 var index=[];
                 var sumArr = 1;
                 var sum =0;
@@ -64,8 +67,15 @@
                 sum = res.data[1];
                    
                 var count = parseInt(sum/$scope.numPages);
-                if(count < sum/$scope.numPages){
+                if (count < sum/$scope.numPages) {
+                    
                     count += 1;
+                    max = count;
+                    for(var i = 1; i<=count; i++){
+                        $scope.pages.push(i);
+                    }
+                } else {
+                    
                     max = count;
                     for(var i = 1; i<=count; i++){
                         $scope.pages.push(i);
@@ -128,6 +138,13 @@
                         var count = parseInt(sum/$scope.numPages);
                         if(count < sum/$scope.numPages){
                             count += 1;
+                            
+                            for(var i = 1; i<=count; i++){
+                                $scope.pages.push(i);
+                            }
+                        } else {
+                            
+                            max = count;
                             for(var i = 1; i<=count; i++){
                                 $scope.pages.push(i);
                             }
@@ -139,7 +156,7 @@
                    // $scope.rsData = [{_id:obj._id, name:obj.name, create:obj.create, deadline:obj.deadline}];
                 });   
             });
-        }
+        } 
     }
 	
     $scope.selectPrevious = function(){
@@ -187,6 +204,13 @@
                         var count = parseInt(sum/$scope.numPages);
                         if(count < sum/$scope.numPages){
                             count += 1;
+                            
+                            for(var i = 1; i<=count; i++){
+                                $scope.pages.push(i);
+                            }
+                        } else {
+                            
+                            max = count;
                             for(var i = 1; i<=count; i++){
                                 $scope.pages.push(i);
                             }
@@ -248,8 +272,15 @@
                        
                     var count = parseInt(sum/$scope.numPages);
                     if(count < sum/$scope.numPages){
+                        
                         count += 1;
                         for(var i = 1; i<=count; i++){
+                            $scope.pages.push(i);
+                        }
+                    } else {
+                            
+                            max = count;
+                            for(var i = 1; i<=count; i++){
                             $scope.pages.push(i);
                         }
                     }
@@ -261,101 +292,6 @@
             });   
         });
     }
-       /* $scope.currentPage = 1;
-        $scope.numPages = 5;
-        $scope.pageSize = 10;
-        $scope.pages = [];*/
-       //获得第一页
-    /*    $http.get(config.'Customers_JSON.txt',
-            {
-                method: 'GET',
-                params: {
-                    'pageNo': $scope.currentPage,
-                    'pageSize': $scope.pageSize
-                },
-                responseType: "json"
-             }).then(function (result) {
-                    $scope.data = result.data.Data;
-                    $scope.numPages = Math.ceil(result.data.Total / result.data.PageSize);
-                });*/
-            /*.success(function(response){
-				$scope.names = response.records;
-				$scope.numPages = Math.ceil($scope.names.length / $scope.pageSize);
-				
-				
-            });*/
-     /*   
-        $scope.onSelectPage = function (page) {
-          
-            $http.get('Customers_JSON.txt',
-                {
-                    method: 'GET',
-                    params: {
-                        'pageNo': page,
-                        'pageSize': $scope.pageSize
-                    },
-                    responseType: "json"
-                }) .then(function (result) {
-                    $scope.data = result.data.Data;
-                    $scope.numPages = Math.ceil(result.data.Total / result.data.PageSize);
-                });
-            /*.success(function(response){
-				$scope.names = response.records;
-				$scope.numPages = Math.ceil($scope.names.length / $scope.pageSize);
-				});*/
-		 //};
-
-		/* app.directive('paging', function() {
-			return {
-            restrict: 'E',
-            //scope: {
-            //    numPages: '=',
-            //    currentPage: '=',
-            //    onSelectPage: '&'
-            //},
-            template: '',
-            replace: true,
-            link: function(scope, element, attrs) {
-                scope.$watch('numPages', function(value) {
-                    scope.pages = [];
-                    for (var i = 1; i <= value; i++) {
-                        scope.pages.push(i);
-                    }
-                    
-                    if (scope.currentPage > value) {
-                        scope.selectPage(value);
-                    }
-                });
-                scope.isActive = function(page) {
-                    return scope.currentPage === page;
-                };
-                scope.selectPage = function(page) {
-                    if (!scope.isActive(page)) {
-                        scope.currentPage = page;
-                        scope.onSelectPage(page);
-                    }
-                };
-                scope.selectPrevious = function() {
-                    if (!scope.noPrevious()) {
-                        scope.selectPage(scope.currentPage - 1);
-                    }
-                };
-                scope.selectNext = function() {
-                    if (!scope.noNext()) {
-                        scope.selectPage(scope.currentPage + 1);
-                    }
-                };
-                scope.noPrevious = function() {
-                    return scope.currentPage == 1;
-                };
-                scope.noNext = function() {
-                    return scope.currentPage == scope.numPages;
-                };
-
-            }
-        };
-    });*/
-
 
 	//deleted
 		$scope.delTask = function(id) {
@@ -363,17 +299,73 @@
                 _id:id
             }
 
-            $http.post(config.apiUrl + 'task/remove', taskId).success(function(response) {
-                alert(response.data)
-                if (response.data) {
-                    $location.path('/task');
-                    $location.replace();
-                } else {
-                    console.log('del error');
-                }
+            $http.post(config.apiUrl + 'task/remove', taskId).then(function(response) {
+                //if (response.data == 'undefined') {
+                  //  $location.path('/task');
+                    //$location.replace();
+                //}
+                 //$scope.rsData.splice($scope.rsData.indexOf(id), 1);
+                alert('删除成功')
+                    $http.get(config.apiUrl + 'user', {
+                    headers: {
+                        Authorization: 'Bearer ' + token
+                    }}).then(function(response){
+                
+                        $scope.currentPage = 1;
+                        $scope.numPages = 5;
+                        $scope.pageSize = 5;
+                        $scope.pages = [];
+                        
+                        var taskId = response.data;
+                        taskId.num = currentPage;
+                    
+                    $http.post(config.apiUrl + 'task/show', taskId).then(function(res){
+                           // $scope.rsData = res.data[0]; 
+                        var index=[];
+                        var sumArr = 1;
+                        var sum =0;
+                        for(var key in res.data[0]){
+                            var obj = res.data[0][key];
+                            for(var key1 in obj){
+                                if (obj.count != sum) {
+                                    obj.count = sumArr*currentPage;
+                                    index.push(obj);
+                                    sum += 1;
+                                } else {
+                                    continue;
+                                }
+                                    
+                            }
+                            sumArr +=1;
+                        }
+                        $scope.rsData = index;
+                        sum = res.data[1];
+                           
+                        var count = parseInt(sum/$scope.numPages);
+                        if (count < sum/$scope.numPages) {
+                            count += 1;
+                            max = count;
+                            for(var i = 1; i<=count; i++){
+                                $scope.pages.push(i);
+                            }
+                        } else {
+                            max = count;
+                            for(var i = 1; i<=count; i++){
+                                $scope.pages.push(i);
+                            }
+                        }
+                        var correntPage = 1;
+                        
+                       // $scope.rsData = [{_id:obj._id, name:obj.name, create:obj.create, deadline:obj.deadline}];
+                    });
+                
+               
+                });
             }).error(function(data,status,headers,config){
 				console.log('no data');
 			})
+
+            
         };
 	
 		
