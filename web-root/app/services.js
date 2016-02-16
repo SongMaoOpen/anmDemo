5d36ad015e3cfb70bacc0e0774f13459a6ec69ce
Sebastian Qu <sebastianqu@gmail.com>
2016-02-16 17:53:17 +0800

    add task service

--- web-root/app/services.js
+++ web-root/app/services.js
@@ -1,11 +1,43 @@
 (function() {
-    var app = angular.module('anmApp.services', []);
+    var app = angular.module('anmApp.services', ['ngResource']);
+
+    var authorizationHeaders = function() {
+        return {
+            Authorization: 'Bearer ' + window.localStorage.getItem('token')
+        };
+    };
 
     app.service('userService', function($http, config) {
         this.getMe = function() {
             return $http.get(config.apiUrl + 'user', {
-                headers: {
-                    Authorization: 'Bearer ' + window.localStorage.getItem('token')
+                headers: authorizationHeaders()
+            });
+        };
+    });
+
+    app.service('taskService', function($resource, config) {
+        this.getFactory =  function() {
+            return $resource(config.apiUrl + 'task/:id', {}, {
+                'get': {
+                    method: 'GET',
+                    headers: authorizationHeaders()
+                },
+                'create': {
+                    method: 'POST',
+                    headers: authorizationHeaders()
+                },
+                'save': {
+                    method: 'PUT',
+                    headers: authorizationHeaders()
+                },
+                'query': {
+                    method: 'GET',
+                    isArray: true,
+                    headers: authorizationHeaders()
+                },
+                'delete': {
+                    method: 'DELETE',
+                    headers: authorizationHeaders()
                 }
             });
         };
